44dd1d4300451c6031c3e1fdb00afacf1eb092dc
Jose Robson <joserobsonsiqueira23@hotmail.com>
2023-05-21 19:12:34 -0300

    feat: docker-compose kibana and elastic

--- pedido/src/main/java/io/github/robinhosz/listener/PedidoRealizadoListener.java
+++ pedido/src/main/java/io/github/robinhosz/listener/PedidoRealizadoListener.java
@@ -1,8 +1,15 @@
 package io.github.robinhosz.listener;
 
 import io.github.robinhosz.dto.PedidoRealizadoDTO;
+import io.github.robinhosz.dto.PratoPedidoDTO;
+import io.github.robinhosz.model.Pedido;
+import io.github.robinhosz.model.Prato;
+import io.github.robinhosz.model.Restaurante;
 import jakarta.enterprise.context.ApplicationScoped;
+import org.bson.types.Decimal128;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
+
+import java.util.ArrayList;
 
 @ApplicationScoped
 public class PedidoRealizadoListener {
@@ -10,5 +17,26 @@ public class PedidoRealizadoListener {
     @Incoming("pedidos")
     public void lerPedidos(PedidoRealizadoDTO pedidoRealizadoDTO) {
         System.out.println(pedidoRealizadoDTO);
+
+        Pedido p = new Pedido();
+
+        //Em breve irei refatorar isso para utilizar o mapstruct ;)
+
+        p.setCliente(pedidoRealizadoDTO.getCliente());
+        pedidoRealizadoDTO.getPratos().forEach(prato -> p.getPratos().add(from(prato)));
+        Restaurante restaurante = new Restaurante();
+        restaurante.setNome(pedidoRealizadoDTO.getRestaurante().getNome());
+        p.setRestaurante(restaurante);
+        p.persist();
+    }
+
+    public Prato from(PratoPedidoDTO pratoPedidoDTO) {
+        Prato p = new Prato();
+        p.setDescricao(pratoPedidoDTO.getDescricao());
+        p.setPreco(new Decimal128(pratoPedidoDTO.getPreco()));
+        p.setNome(pratoPedidoDTO.getNome());
+
+        return p;
+
     }
 }
